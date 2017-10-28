1762fa897416ffb44375d3e876733190a202788a
threegirls89 <threegirls89.asm@gmail.com>
2017-10-28 21:32:47 +0900

    枠，目盛線，プロットに色を追加

--- tgepspreamble.py
+++ tgepspreamble.py
@@ -1,25 +1,24 @@
-#!/usr/bin/env python
 import codecs
 
 fin = codecs.open('tgepspreamble.eps', 'r', 'utf_8')
 fout = codecs.open('tgepspreamble.sty', 'w', 'utf_8')
 writable = False
 for str in fin:
-    if writable:
-        str = str.strip(' \t\n')
-        if len(str) > 0 and str[0] != '%':
-            idx = str.find('%')
-            idx = None if  idx == -1 else idx
-            str = str[0 : idx]
-            str = str.replace('{', '\\@charlb ')
-            str = str.replace('}', '\\@charrb ')
-            fout.write('\\immediate\\write\\tg@epsfile{' + str + '}%\n')
-        elif str == '% endinput':
-            fout.write('}\n')
-            break
-    elif str == '% begininput\n':
-        fout.write('\\def\\tg@epspreamble{%\n')
-        writable = True
+	if writable:
+		str = str.strip(' \t\n')
+		if len(str) > 0 and str[0] != '%':
+			idx = str.find('%')
+			idx = None if  idx == -1 else idx
+			str = str[0 : idx]
+			str = str.replace('{', '\\@charlb ')
+			str = str.replace('}', '\\@charrb ')
+			fout.write('\\immediate\\write\\tg@epsfile{' + str + '}%\n')
+		elif str == '% endinput':
+			fout.write('}\n')
+			break
+	elif str == '% begininput\n':
+		fout.write('\\def\\tg@epspreamble{%\n')
+		writable = True
 fin.close()
 fout.close()
-
+print("abgeschlossen")
